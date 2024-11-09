6057e72506ffd606e90d7c7905d405256f7267b3
Bhushan828 <96247554+Bhushan828@users.noreply.github.com>
2024-11-09 06:39:22 +0000

    datatypes

---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+let name = "bhushan"
+let age = 23
+let isloggedIn = true
+let state;
+
+// number => 2 to power 23
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol => unique
+
+
+
+//boject
+
+console.log(typeof null);  // object
+console.log(typeof name);  // string
+console.log(typeof state );// undefined 
+
+
+
+
